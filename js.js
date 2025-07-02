b010b15457be94182758482737566f23f3db8d49
Taisiya-Gerasimova-QA <taisiya-semenova@yandex.ru>
2025-07-02 17:12:58 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+var button = document.querySelector ('button');
+
+    button.addEventListener('click', function() {
+      alert('Обратная связь скоро будет! Свяжитесь с нами по телефону или email.');
+    })
